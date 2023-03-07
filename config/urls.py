00f6b7648c7e38ce8999e82d7297bdd86897391f
kaietanpaul <kaietanpaul@gmail.com>
2023-03-07 17:05:59 +0100

    courses models better support and media settings

--- config/urls.py
+++ config/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     path('', include('home.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
     path('users/', include('users.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+              # + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
